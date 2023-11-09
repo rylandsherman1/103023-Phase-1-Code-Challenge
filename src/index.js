b48199ac39f7116ea02d4fc4fb1c1c746ee6a70a
Ryland Sherman <rylandsherman1@gmail.com>
2023-11-08 18:00:27 -0700

    submission Deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
-// Your code here
+const movieAPI = 'http://localhost:3000';
+
+const moviePoster = el('poster');
+const movieTitle = el('title');
+const movieRuntime = el('runtime');
+const movieShowtime = el('showtime');
+const movieTickets = el('ticket-num');
+const movieDescription = el('film-info');
+const movieList = el('films');
+
+el('buy-ticket').addEventListener('click', buyTicket);
+
+fetch(`${movieAPI}/1`)
+    .then(res => res.json())
+    .then(renderMovie)
+
+fetch(`${movieAPI}`)
+    .then(res => res.json())
+    .then(renderMovieList)
+
+function renderMovieList(movies) {
+    movieList.innerHTML = '';
+    movies.forEach(renderList);
+}
+
+function renderMovie(movie) {
+    movieTitle.textContent = movie.title; 
+    moviePoster.src = movie.poster;  
+    movieRuntime.textContent = `${movie.runtime} minutes`;  
+    movieShowtime.textContent = movie.showtime;  
+}
+
+function renderList(movie) {
+    const movieLi = document.createElement('li'); 
+    movieLi.textContent = movie.title;
+    movieLi.addEventListener('click', () => renderMovie(movie)); 
+    movieList.append(movieLi);
+}
+
+function buyTicket() {
+    const availableTicketsElement = document.getElementById('ticket-num');
+    let availableTickets = parseInt(availableTicketsElement.textContent);
+  
+    if (availableTickets > 0) {
+      availableTickets--;
+      availableTicketsElement.textContent = availableTickets;
+    }
+}
+
+function el(id) {
+    return document.getElementById(id);
+}
+
+
